3ee80abe0cb5a14bc4b8f63ce4a942ea59d82aa8
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-03-17 18:46:58 +0100

    display SSID IP address if ours

--- iw-scan.pl
+++ iw-scan.pl
@@ -6,6 +6,31 @@ use autodie;
 use Socket qw( inet_aton );
 use Data::Dump qw(dump);
 
+my $mac2ip;
+
+foreach my $file ( glob "out/*ip?addr" ) {
+
+	my $macs;
+	my $addr;
+
+	open(my $fh, '<', $file);
+	while(<$fh>) {
+		chomp;
+		if ( m/link\/ether ([0-9a-f:]+)/ ) {
+			$macs->{$1}++;
+		} elsif ( m/inet ([0-9\.]+)/ ) {
+			$addr = $1;
+		}
+	}
+
+	foreach my $m ( keys %$macs ) {
+		$mac2ip->{$m} = $addr;
+	}
+}
+
+
+#warn "# mac2ip = ",dump($mac2ip);
+
 my $mac;
 my $wifi;
 
@@ -32,16 +57,17 @@ foreach my $file ( sort glob "out/*.iw*scan" ) {
 	}
 }
 
-my $fmt = "%s %s %-20s %2s %4s %-6s %-10s\n";
-printf $fmt ,"IP","mac","SSID","ch","Freq","Signal","Encryption";
+my $fmt = "%15s %17s %15s %-20s %2s %4s %-6s %-10s\n";
+printf $fmt ,"IP","mac","remote IP","SSID","ch","Freq","Signal","Encryption";
 
-warn "# wifi = ",dump($wifi);
+#warn "# wifi = ",dump($wifi);
 
 foreach my $m ( sort { inet_aton($wifi->{$a}->{ip}) cmp inet_aton($wifi->{$b}->{ip}) } keys %$wifi ) {
 
 	printf $fmt,
 		$wifi->{$m}->{ip}, 
 		$m,
+		$mac2ip->{$m} || '?',
 		$wifi->{$m}->{ssid}, 
 		$wifi->{$m}->{channel} || '?', 
 		$wifi->{$m}->{freq}, 
