71f1e87a742a05a1b355bb34425f5bd976e14c0b
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-03-17 18:27:15 +0100

    show ip from filename and sort by it

--- iw-scan.pl
+++ iw-scan.pl
@@ -3,25 +3,28 @@ use warnings;
 use strict;
 use autodie;
 
+use Socket qw( inet_aton );
 use Data::Dump qw(dump);
 
 my $mac;
 my $wifi;
 
 foreach my $file ( sort glob "out/*.iw*scan" ) {
+	my $ip = $1 if ( $file =~ m/out\/(.+?)\.iw/ );
 	open(my $fh, '<', $file);
 	while(<$fh>) {
 		chomp;
 		if ( m/BSS ([0-9a-f:]+)/ ) {
 			$mac = $1;
 			$wifi->{$mac}->{enc} = 'Open';
+			$wifi->{$mac}->{ip} = $ip;
 		} elsif ( m/SSID: (\S+)/ ) {
 			$wifi->{$mac}->{ssid} = $1;
 		} elsif ( m/freq: (\S+)/ ) {
 			$wifi->{$mac}->{freq} = $1;
 		} elsif ( m/signal: (\S+)/ ) {
 			$wifi->{$mac}->{sig} = $1;
-		} elsif ( m/Authentication suites: (\S+)/ ) {
+		} elsif ( m/Authentication suites: (.+)/ ) {
 			$wifi->{$mac}->{enc} = $1;
 		} elsif ( m/primary channel: (\d+)/ ) {
 			$wifi->{$mac}->{channel} = $1;
@@ -29,16 +32,18 @@ foreach my $file ( sort glob "out/*.iw*scan" ) {
 	}
 }
 
-my $fmt = "%17s %-20s %2s %4s %-6s %-10s\n";
-printf $fmt ,"mac","SSID","ch","Freq","Signal","Encryption";
+my $fmt = "%s %s %-20s %2s %4s %-6s %-10s\n";
+printf $fmt ,"IP","mac","SSID","ch","Freq","Signal","Encryption";
 
 warn "# wifi = ",dump($wifi);
 
-foreach my $m ( sort keys %$wifi ) {
+foreach my $m ( sort { inet_aton($wifi->{$a}->{ip}) cmp inet_aton($wifi->{$b}->{ip}) } keys %$wifi ) {
 
-	printf $fmt, $m,
+	printf $fmt,
+		$wifi->{$m}->{ip}, 
+		$m,
 		$wifi->{$m}->{ssid}, 
-		$wifi->{$m}->{channel}, 
+		$wifi->{$m}->{channel} || '?', 
 		$wifi->{$m}->{freq}, 
 		$wifi->{$m}->{sig}, 
 		$wifi->{$m}->{enc}, 
