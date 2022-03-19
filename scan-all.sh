aff9bb3b9e7c6bd55c01f4fc5c3c84574830d30e
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-03-19 09:33:17 +0100

    using parallel

--- scan-all.sh
+++ scan-all.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -xe
 
-./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan0 scan
-./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan1 scan
-./wap-ips | xargs -i ./wifi-ssh-out {} ip addr
-./wap-ips | xargs -i ./wifi-ssh-out {} iw dev
+#./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan0 scan
+#./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan1 scan
+
+./wap-ips | parallel -j 75 './wifi-ssh-out {} iw wlan0 scan' 
+./wap-ips | parallel -j 75 './wifi-ssh-out {} iw wlan1 scan'
+
+#./wap-ips | xargs -i ./wifi-ssh-out {} ip addr
+#./wap-ips | xargs -i ./wifi-ssh-out {} iw dev
+
+git -C out commit -m $( date +%Y-%m-%dT%H:%M:%S ) -a
