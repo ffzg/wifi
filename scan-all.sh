6e94d77e58cae1f4a142d9497e4cd8f9ea25a32f
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-03-24 18:27:21 +0100

    arguments: dev, uci show

--- scan-all.sh
+++ scan-all.sh
@@ -3,10 +3,25 @@
 #./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan0 scan
 #./wap-ips | xargs -i ./wifi-ssh-out {} iw wlan1 scan
 
+if [ -z "$1" ] ; then
+
 ./wap-ips | parallel -j 75 './wifi-ssh-out {} iw wlan0 scan' 
 ./wap-ips | parallel -j 75 './wifi-ssh-out {} iw wlan1 scan'
 
-#./wap-ips | xargs -i ./wifi-ssh-out {} ip addr
+elif [ "$1" = "dev" ] ; then
+
 #./wap-ips | xargs -i ./wifi-ssh-out {} iw dev
+./wap-ips | parallel -j 75 './wifi-ssh-out {} iw dev'
+
+elif [ "$*" = 'uci show' ] ; then
+
+	./wap-ips | parallel -j 75 './wifi-ssh-out {} uci show'
+
+else
+	echo "Unsupported command: $*"
+	exit 1
+fi
+
+#./wap-ips | xargs -i ./wifi-ssh-out {} ip addr
 
 git -C out commit -m $( date +%Y-%m-%dT%H:%M:%S ) -a
